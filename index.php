cbf43762518ac49589b4dfac27d732a701cf477f
kahy <pepa.kahy@gmail.com>
2020-10-21 16:26:30 +0200

    Updated styles + scripts(form + map)

--- index.php
+++ index.php
@@ -1,36 +1,54 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <!-- css -->
-    <link rel="stylesheet" href="index.css">
-    <!-- map javascript -->
-    <script src="index.js"></script>
-    <title>Kde domov můj</title>
-    <link rel="icon" type="image/png" href="images/find-home.ico">
-    <!-- Radši bych to tady nechal pro jistotu-->
-    <!-- <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>-->
 
-	  <!-- map API -->
-	  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqac8iFjKYK_AIRaoRxXfYaTJE3CSkmUI&callback=initMap&libraries=visualization&v=weekly" defer></script>
-  </head>
-  <body>
-    <div id = "navbar">
-          <img src="images/find-home-web" height="100%"></img>
-          <ul>
-          <li><a href="#">o nás</a></li>
-          <li><a href="#">kontakt</a></li>
-          <li><a href="#">mapa</a></li>
-          </ul>
-    </div>
+<head>
+  <!-- css -->
+  <link rel="stylesheet" href="index.css">
+  <!-- map javascript -->
+  <script src="scripts/form.js"></script>
+  <script src="scripts/mapa.js"></script>
+  <title>Find home</title>
+  <link rel="icon" type="image/png" href="images/find-home.ico">
 
-    <div id="map">
-    </div>
-    <div id="map-settings">
+  <!-- Radši bych to tady nechal pro jistotu-->
+  <!-- <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>-->
+
+  <!-- map API -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqac8iFjKYK_AIRaoRxXfYaTJE3CSkmUI&callback=initMap&libraries=visualization&v=weekly" defer></script>
+</head>
+
+<body>
+  <div id="navbar">
+    <img src="images/find-home-web" height="100%"></img>
     <ul>
-          <li><a href="#">o nás</a></li>
-          <li><a href="#">kontakt</a></li>
-          <li><a href="#">mapa</a></li>
-          </ul>
-    </div>
-  </body>
+      <li><a href="#map">MAPA</a></li>
+      <li><a href="#">O PROJEKTU</a></li>
+      <li><a href="#">KONTAKT</a></li>
+    </ul>
+  </div>
+
+  <div id="form">
+    <ul id="checkbox">
+      <li> <input type="checkbox" class="check"></input></li>
+      <li> <input type="checkbox" class="check"></input></li>
+      <li> <input type="checkbox" class="check"></input></li>
+      <li> <input type="checkbox" class="check"></input></li>
+      <li> <input type="checkbox" class="check"></input></li>
+    </ul>
+    <div type="button" id="comp" onclick="comp()">Vyhodnoť</div>
+  </div>
+
+  <div id="map">
+  </div>
+  <div id="map-settings">
+    <ul>
+      <li><a onclick="changeOpacity()">opacity</a></li>
+    </ul>
+  </div>
+<?php
+echo "helou, tady bude ta pravá šmakuláááááááda";
+?>
+
+</body>
+
 </html>
---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+let points = 0;
+
+const checkBox = document.getElementsByClassName("check");
+const form = document.getElementById("form");
+const btn = document.getElementById("comp");
+
+function comp() {
+    
+}
+
